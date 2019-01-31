4d1a8c1956c89f777384685a2f42d996d7e3afec
dianlujitao <dianlujitao@lineageos.org>
2019-01-31 22:26:17 +0800

    clover: Use libcryptfs_hw from commonsys repo
    Change-Id: I342f57e9e66cd4f6674d7ce318a2a8dabebef8c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
 
 # Crypto
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Kernel
