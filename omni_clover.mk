81aeb68d1e0f033c03da91d4e53b9205b06b7779
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-28 12:22:57 +0800

    Init twrp for clover
    Change-Id: Ia41e1fcc6e817da12216c8ff689aa80146f93b03

---
+++ omni_clover.mk
@@ -0,0 +1,40 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := clover
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=sdm660 \
+    sys.usb.controller=a800000.dwc3 \
+    sys.usb.rndis.func.name=rndis_bam \
+    sys.usb.rmnet.func.name=rmnet_bam
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := clover
+PRODUCT_NAME := omni_clover
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI PAD 4
+PRODUCT_MANUFACTURER := Xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := clover
+TARGET_VENDOR_DEVICE_NAME := clover
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=clover PRODUCT_NAME=clover
