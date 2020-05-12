ff9eec2589f1c7cacadd64a9fd7f982339c96257
Max Weffers <rcstar6696@gmail.com>
2020-05-12 22:06:44 +0200

    clover: Set Correct Tap to Wake node for HMP Power Hal
    Change-Id: Ie75961c79745d796afe105946354c52c15f2d923

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,9 @@ include device/xiaomi/sdm660-common/BoardConfigCommon.mk
 # Device Path
 DEVICE_PATH := device/xiaomi/clover
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchscreen/enable_dt2w"
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
